0464426e372553bbc87ce60aca5176ae9fbffce2
eschul07 <56516414+eschul07@users.noreply.github.com>
2019-11-10 12:08:00 -0800

    Update accel-LED-interface.ino

--- Code/accel-LED-interface.ino
+++ Code/accel-LED-interface.ino
@@ -1,21 +1,27 @@
+/* What I have so far. 
+Determines cartesian direction (in radians) and magnitude 
+of input vector from ADXL345 in the form: x, y (ex: 25,-53).
+Need to add categorization for magnitude and direction,
+to select particular LED and color. */
+
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-float x = 3;    // Example of x component of input vector
-float y = -4;   // Example of x component of input vector
+float x = 3;    // Example of x-component of input vector
+float y = -4;   // Example of y-component of input vector
 float angle;    // angle variable
-float mag;      // angle variable
+float mag;      // magnitude variable
 
-if (x>0) {              // If x component is within cartesian quadrant 1 or 4
+if (x>0) {              // If x-component is within cartesian quadrant 1 or 4
   angle = atan(x/y);    // (In radians)
 }
-else {                  // If x component is within cartesian quadrant 2 or 3
+else {                  // If x-component is within cartesian quadrant 2 or 3
   angle = atan(x/y) + PI; // (In radians)
 }
 
 mag = sqrt(sq(x)+sq(y));  // Determines magnitude (independent of cartesian quadrant)
 
-Serial.println(angle, 8); // Display angle (in radians)
+Serial.println(angle, 8); // Display direction (in radians)
 Serial.println(mag, 4);   // Display magnitude
 
 }
