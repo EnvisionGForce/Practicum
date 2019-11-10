7d6c0850cd52ef971e944bdfef729e66ef0ed95c
eschul07 <56516414+eschul07@users.noreply.github.com>
2019-11-10 12:00:00 -0800

    Update accel-LED-interface.ino

--- Code/accel-LED-interface.ino
+++ Code/accel-LED-interface.ino
@@ -1,22 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-float x = 3;
-float y = -4;
-float angle;
-float mag;
+float x = 3;    // Example of x component of input vector
+float y = -4;   // Example of x component of input vector
+float angle;    // angle variable
+float mag;      // angle variable
 
-if (x>0) {
-  angle = atan(x/y);
+if (x>0) {              // If x component is within cartesian quadrant 1 or 4
+  angle = atan(x/y);    // (In radians)
 }
-else {
-  angle = atan(x/y) + PI;
+else {                  // If x component is within cartesian quadrant 2 or 3
+  angle = atan(x/y) + PI; // (In radians)
 }
 
-mag = sqrt(sq(x)+sq(y));
+mag = sqrt(sq(x)+sq(y));  // Determines magnitude (independent of cartesian quadrant)
 
-Serial.println(angle, 8);
-Serial.println(mag, 4);
+Serial.println(angle, 8); // Display angle (in radians)
+Serial.println(mag, 4);   // Display magnitude
 
 }
 
